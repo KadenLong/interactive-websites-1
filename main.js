6387afafd7bede51a0ace32852973c6a1681f088
Kaden Long <kglong20@gmail.com>
2022-02-02 16:12:56 -0700

    completed assignment, all features working

---
+++ main.js
@@ -0,0 +1,52 @@
+const message = document.querySelector('#message');
+
+const addMovie = (event) => {
+event.preventDefault()
+const inputField = document.querySelector('input');
+const movie = document.createElement('li');
+const movieTitle = document.createElement('span');
+movieTitle.textContent = inputField.value;
+movieTitle.addEventListener('click', crossOffMovie);
+movie.appendChild(movieTitle);
+
+const deleteBtn = document.createElement('button');
+deleteBtn.textContent = 'X';
+deleteBtn.addEventListener('click', deleteMovie);
+movie.appendChild(deleteBtn);
+
+document.querySelector('ul').appendChild(movie);
+inputField.value = ""
+}
+
+document.querySelector('form').addEventListener('submit', addMovie);
+
+const deleteMovie = (event) => {
+    event.target.parentNode.remove()
+    message.textContent = `${event.target.parentNode.firstChild.textContent} Deleted!`
+    revealMessage();
+} 
+
+//Step 4//
+
+const crossOffMovie = (event) => {
+    event.target.classList.toggle('checked');
+
+    if (event.target.classList.contains('checked')){
+        message.textContent = `${event.target.textContent} Watched!`
+    } else {
+        message.textContent = `${event.target.textContent} Added Back!`
+    }
+
+    revealMessage()
+}
+
+//Intermediate//
+
+const revealMessage = () => {
+    message.classList.remove('hide')
+    setTimeout(() => {
+        message.classList.add('hide');
+    }, 1000);
+}
+
+
